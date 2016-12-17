7c69d636142ce3236c227c09b9d29964d1285af6
Juhana Leinonen <git@nitku.net>
2016-12-17 23:07:19 +0200

    About tab

--- www/index.php
+++ www/index.php
@@ -2,9 +2,12 @@
 <html lang="en">
 <head>
     <title>HugoJS – Hugo Online Interpreter</title>
+
     <meta charset="UTF-8">
     <meta name="description" content="Play Hugo interactive fiction online with the HugoJS interpreter">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <base target="_parent" />
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="gallery.css">
@@ -31,6 +34,11 @@
         <li role="presentation">
             <a href="#link" aria-controls="link" role="tab" data-toggle="tab">
                 Link
+            </a>
+        </li>
+        <li role="presentation">
+            <a href="#about" aria-controls="link" role="tab" data-toggle="tab">
+                About
             </a>
         </li>
     </ul>
@@ -70,6 +78,13 @@
             "ifdb" => "x1qqstrlsa9ocr0j"
         ),
         array(
+            "title" => "Party Arty, Man of La Munchies",
+            "author" => "Jonathan Blask",
+            "file" => "party",
+            "info" => "A story told at the crossroads of levity and destiny!",
+            "ifdb" => "ht6lb6z7qnklrv4q"
+        ),
+        array(
             "title" => "Renga in Four Parts",
             "author" => "Jason Dyer",
             "file" => "renga",
@@ -94,7 +109,7 @@
             "title" => "Tales of a Clockwork Boy",
             "author" => "Taleslinger",
             "file" => "clockworkboy",
-            "info" => "Once upon a time, there was a queen of fay blood who couldn’t bear children. To humor her saddened king, she learnt the secret arts of alchemy and mechanicks, and her head bore what her womb would not. But even that clockwork boy, a wonder held in awe throughout the kingdom, couldn’t lift the king's spirits, and he fell into a great sadness.",
+            "info" => "Once upon a time, there was a queen of fay blood who couldn't bear children. To humor her saddened king, she learnt the secret arts of alchemy and mechanicks, and her head bore what her womb would not. But even that clockwork boy, a wonder held in awe throughout the kingdom, couldn't lift the king's spirits, and he fell into a great sadness.",
             "ifdb" => "uoyqgvkalgnhzs1n"
         ),
         array(
@@ -107,7 +122,7 @@
     );
 
     for( $i = 0; $i < count( $games ); ++$i ) {
-        $playUrl = "play/?story=library/".$games[$i]["file"].".hex";
+        $playUrl = "http://textadventures.online/play/?story=library/".$games[$i]["file"].".hex";
 ?>
         <div class="list-group-item">
             <div class="row">
@@ -163,7 +178,67 @@
                 </button>
             </form>
         </div>
+        <div id="about" role="tabpanel" class="tab-pane">
+			<p>
+				<strong>HugoJS</strong> is an online interpreter for games made
+				with the Hugo interactive fiction authoring system.
+				Source code and instructions on how to install it on your own
+				web site can be found from its
+				<a href="https://github.com/juhana/hugojs">GitHub repository</a>.
+			</p>
+            <p>
+                <strong>Hugo</strong> was first released by author
+                <a href="http://www.kenttessman.com">Kent Tessman</a> in 1995.
+                It was designed largely as a response to the IF system giants of
+                the day, TADS and Inform 6, the latter being the popular IF
+                system that compiled games to Infocom's own Z-machine. Hugo
+                mirrored several of their concepts but added its own bent.
+			</p>
+			<p>
+                Its manual, The Hugo Book, reads: "Hugo does hope to extend the
+                concepts developed in earlier, similar systems in order to make
+                interactive fiction programming less cryptic, and more flexible
+                and accessible to designers, as well as to add functionality in
+                certain areas where other systems are lacking." Later releases
+                added multimedia support and other improvements.
+            </p>
+            <p>
+                While Hugo never achieved the popularity of the systems it aimed
+                to replace, the general consensus of the IF community has always
+                been that it is a solid choice for anyone looking to write a
+                game with a smart parser and library. Moreover, some people
+                prefer its blend of coding and world model design to this day
+                (which isn't to say that <a href="http://inform7.com">Inform</a>
+                and <a href="http://tads.org">TADS</a> haven't had their own
+                permutations over the years).
+			</p>
+			<p>
+                Those interested in writing their own Hugo games have several
+                online resources at their disposal:
+                <ul>
+                	<li>
+                		<a href="http://hugo.gerynarsabode.org/index.php?title=Getting_Started">Hugo By Example's "Getting Started" page</a>
+					</li>
+					<li>
+                		<a href="http://notdeadhugo.blogspot.com">Not Dead Hugo</a>
+					</li>
+					<li>
+                		<a href="http://www.joltcountry.com/phpBB2/viewforum.php?f=8">The joltcountry.com Hugo discussion forum</a>
+					</li>
+                </ul>
+            </p>
+        </div>
     </div>
 </div>
+<script>
+$(function() {
+	if( location.href.indexOf( '?link' ) > -1 ) {
+		$( '.nav-tabs a[href="#link"]' ).click();
+	}
+	else if( location.href.indexOf( '?about' ) > -1 ) {
+		$( '.nav-tabs a[href="#about"]' ).click();
+	}
+});
+</script>
 </body>
 </html>
